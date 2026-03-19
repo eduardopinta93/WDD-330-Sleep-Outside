69abb6c92dd4eecbd822ecfc871ae28c0a06c847
eduardopinta93 <eduardo.pintam@gmail.com>
2026-03-19 14:10:37 +0100

    Dynamic header/footer

--- src/js/product.js
+++ src/js/product.js
@@ -1,6 +1,7 @@
 import { getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 import ProductDetails from "./ProductDetails.mjs";
+import {loadHeaderFooter} from "./utils.mjs";
 
 const productId = getParam("product");
 const dataSource = new ProductData("tents");
@@ -8,6 +9,7 @@ const dataSource = new ProductData("tents");
 const product = new ProductDetails(productId, dataSource);
 product.init();
 
+loadHeaderFooter();
 
 //import { getParam } from "./utils.mjs";
 //import ProductData from "./ProductData.mjs";
